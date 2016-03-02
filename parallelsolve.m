6c3b36209c1d534be39e7564cbe6f2992105daea
astuke <annika.stuke@aalto.fi>
2016-03-02 17:38:35 +0200

    Added files via upload

---
+++ parallelsolve.m
@@ -0,0 +1,36 @@
+[t,Y]=ode45('parallel',[0, 10],[ 1 2 3 4 5 6]); %call the file
+
+y1=Y(:,1);
+y2=Y(:,2);
+y3=Y(:,3);
+y4=Y(:,4);
+y5=Y(:,5);
+y6=Y(:,6);
+
+
+
+hold on;
+figure(4);
+plot(t,y2,'Color', [0 0 1], 'LineWidth', 3);
+hold on;
+plot(t,y1,'Color', [0 0.6 1], 'LineWidth', 3);
+hold on;
+plot(t,y3,'Color', [0 0.9 1], 'LineWidth', 3);
+h=xlabel('t'); g=ylabel('x, y, z');
+set(gca, 'FontSize', 40, 'linewidth',3);
+set(legend, 'FontSize', 20);
+set(h, 'Fontsize', 40);
+set(g, 'Fontsize', 40);
+set(legend, 'FontSize', 30);
+
+hold on;
+figure(18);
+plot3(y1,y2,y3, 'Color', [0 0 1], 'LineWidth', 2);
+h=xlabel('x'); g=ylabel('y'); j=zlabel('z');
+set(gca, 'FontSize', 15, 'linewidth',3, 'FontSize', 30);
+set(h, 'Fontsize', 30);
+set(g, 'Fontsize', 30);
+set(j, 'Fontsize', 30);
+box on;
+grid on;
+rotate3d on;
