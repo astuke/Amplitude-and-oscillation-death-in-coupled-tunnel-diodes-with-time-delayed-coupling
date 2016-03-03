91487a38a52a7c1002446552a889c58819b3ced6
Annika Stuke <annika.stuke@aalto.fi>
2016-03-03 19:24:38 +0200

    Update seriesdelay.m

--- series_circuit/seriesdelay.m
+++ series_circuit/seriesdelay.m
@@ -1,13 +1,13 @@
 function dY=seriesdelay(t,Y,Z)
-zlag1=Z(:,1);
+%create arrays with delayed time Z --> specified in seriesdelaysolve.m
+zlag1=Z(:,1); 
 zlag2=Z(:,2);
 dY=zeros(6,1);
-
 
 a2=1.35;
 kappa=0.08;
 
-% physical params
+% physical parameters
 rho=86;
 RL=1;
 RB=2000;  
@@ -21,26 +21,25 @@ C=56*10^(-12);
 Cd1=26*10^(-12);
 Cd2=1.1*Cd1;
 
-% nondimensional params
+% nondimensional parameters
 dL=RL/rho;
 dB=RB/rho;
 d=kappa*dB;
 eps=(rho*rho*Cd1)/Ld;
 gamma =R*C*rho/Ld;
 
-x1=Y(1);
-y1=Y(2);
-z1=Y(3);
-x2=Y(4);
-y2=Y(5);
-z2=Y(6);
+% 6 variables:
+x1=Y(1); %voltage across internal capacitor of first TD
+y1=Y(2); %current through first TD 
+z1=Y(3); %voltage across external capacitor of first TD
+x2=Y(4); %voltage across internal capacitor of second TD
+y2=Y(5); %current through second TD 
+z2=Y(6); %voltage across external capacitor of second TD
 
-[eps dL   aL   gamma   d   a2 kappa]
-[x1 y1 z1 x2 y2 z2]
-    
+%dynamical system of two delayed coupled tunnel diodes in parallel
 dY(1)=1/eps*(x1-x1^3/3-y1);
 dY(2)=x1+z1-dL*y1-aL;
-dY(3)=1/gamma*(a2-z1-z2-d*y1-kappa*(z1-zlag1(6)));
+dY(3)=1/gamma*(a2-z1-z2-d*y1-kappa*(z1-zlag2(6))); % time-delayed coupling between z variables of the two TDs
 dY(4)=1/eps*(x2-x2^3/3-y2);
 dY(5)=x2+z2-dL*y2-aL;
-dY(6)=1/gamma*(a2-z2-z1-d*y2-kappa*(z2-zlag2(3)));
+dY(6)=1/gamma*(a2-z2-z1-d*y2-kappa*(z2-zlag1(3))); % time-delayed coupling between z variables of the two TDs
