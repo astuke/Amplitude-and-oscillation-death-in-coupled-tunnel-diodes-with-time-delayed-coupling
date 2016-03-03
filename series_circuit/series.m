1592c97285604e19645dc91099ec8aac8dc5b00c
Annika Stuke <annika.stuke@aalto.fi>
2016-03-03 18:47:12 +0200

    Update series.m

--- series_circuit/series.m
+++ series_circuit/series.m
@@ -1,10 +1,9 @@
-function dY=series(t,Y,eps,a,d,gamma)
+function dY=series(t,Y,eps,a,d,gamma) %solve with seriessolve.m
 dY=zeros(6,1); 
+
+% physical parameters
 a2=0;
 d=0.57;
-
-
-% physical params
 rho=86;
 RL=1; 
 R=d*rho
@@ -17,23 +16,20 @@ C=56*10^(-12);
 Cd1=26*10^(-12);
 Cd2=1.1*Cd1;
 
-% nondimensional params
+% nondimensional parameters
 dL=RL/rho;
 eps=(rho*rho*Cd1)/Ld;
 gamma =R*C*rho/Ld;
 
+%6 variables (3 for each tunnel diode):
+x1=Y(1); %voltage across internal capacitor of first TD
+y1=Y(2); %current through first TD 
+z1=Y(3); %voltage across external capacitor of first TD
+x2=Y(4); %voltage across internal capacitor of second TD
+y2=Y(5); %current through second TD 
+z2=Y(6); %voltage across external capacitor of second TD
 
-
-x1=Y(1);
-y1=Y(2);
-z1=Y(3);
-x2=Y(4);
-y2=Y(5);
-z2=Y(6);
-
-[eps dL   aL   gamma   d   a2]
-[x1 y1 z1 x2 y2 z2]
-    
+%dynamical system of two tunnel diodes coupled in series   
 dY(1)=1/eps*(x1-x1^3/3-y1);
 dY(2)=x1+z1-dL*y1-aL;
 dY(3)=1/gamma*(a2-z1-z2-d*y1);
