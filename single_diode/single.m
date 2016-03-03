775563f31da2bbde70b115a830639b5bb4460f5c
Annika Stuke <annika.stuke@aalto.fi>
2016-03-03 18:24:45 +0200

    Update single.m

--- single_diode/single.m
+++ single_diode/single.m
@@ -12,9 +12,10 @@ I0=0.00122;
 K=0.1306;
 aL=RL*I0/K;
 
-x=Y(1);
-y=Y(2);
-z=Y(3);
+%the system has 3 variables:
+x=Y(1); %voltage across internal capacitance
+y=Y(2); %current through tunnel diode
+z=Y(3); %voltage across external capacitor
     
 dY(1)=1/eps*(x-x^3/3-y); %dynamical eqations describing circuit with one tunnel diode
 dY(2)=x+z-dL*y-aL;
